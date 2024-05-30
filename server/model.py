c8434161f30bc39116bf0fed8ec198285e9e9897
BebekJK <kevinjk2802@gmail.com>
2024-05-31 02:17:57 +0800

    feat: add basic inference model

--- server/model.py
+++ server/model.py
@@ -4,16 +4,27 @@ from langchain_openai import ChatOpenAI
 import os
 import dotenv
 
+dotenv.load_dotenv()
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 OPENAI_MODEL = "gpt-3.5-turbo-0125"
+TEMPERATURE = 0
 
 def main():
     # Setup the LLM
-    dotenv.load_dotenv()
-    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-    openai_chat = ChatOpenAI(
+    llm = ChatOpenAI(
         model=OPENAI_MODEL,
+        temperature=TEMPERATURE,
         api_key=OPENAI_API_KEY)
     
+    messages=[
+        ("system", "Hello you are a 50 years old dad, and so you are an expert in a dad jokes!"),
+        ("human", "Give me a hilarious dad joke!"),
+    ]
+        
+    response = llm.invoke(messages)
+    print(f"Response: {response.content} \n")
+    print(f"Using {response.response_metadata['token_usage']['total_tokens']} tokens for this prompt.")
+
 if __name__=="__main__":
     main()
     
