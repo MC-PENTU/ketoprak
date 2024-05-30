17f3abdd978515d708203e0c427c8e1723b37f05
BebekJK <kevinjk2802@gmail.com>
2024-05-31 01:21:33 +0800

    update readme

--- server/test.py
+++ server/test.py
@@ -1,6 +1,8 @@
 import os
 import dotenv
 
-# Check if the .env file exists
-dotenv.load_dotenv()
-print(os.getenv('OPENAI_API_KEY'))
+
+if __name__=="__main__":
+    # Check if the .env file exists
+    dotenv.load_dotenv()
+    print("Your OpenAI API Key: ", os.getenv('OPENAI_API_KEY'))
